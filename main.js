1bacfb4b7d199c69cdeedfa1c06e67a22033c4f3
Deron <Deron@relevantdevelopment.tech>
2019-03-27 14:35:04 -0400

    many changes

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+phpResponse = "hello!"; 
+
 function lookupFlights(){
-    
+    alert("Looking up flights!"); 
 }
+
+function getCityCode(){
+    document.getElementById("cityCodePrompt").style.display="flex"; 
+}
+
+function hidePrompt(){
+    document.getElementById('cityCodePrompt').style.display='none'; 
+}
+
+function fillOriginIATA(){
+    hidePrompt(); 
+}
+
+function fillIATACodes(){
+    var ajax = new XMLHttpRequest(); 
+    var method = "GET"; 
+    var url = "/php/getCities.php"; 
+    var asynchronous = true; 
+
+    ajax.open(method, url, asynchronous);
+    ajax.send(); 
+
+    ajax.onreadystatechange=function(){
+        if(this.readyState==4 && this.status ==200){
+            alert(this.responseText); 
+        }
+    }
+}
+
+
+
---
+++ php/tempCodeRunnerFile.php
@@ -0,0 +1,6 @@
+<?php
+unirest.get("https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=50&lng=-157.895277&lat=21.265600")
+    .header("X-RapidAPI-Key", "c1f5d511abmshcafd4c34fd8b398p16de3djsn56bfb5538d50")
+    .end(function (result) {
+      console.log(result.status, result.headers, result.body);
+    });
